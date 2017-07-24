1066b9bda39abd6a7ed06a9b5526261eec8c59e3
kitralzhong <784248377@qq.com>
2017-07-24 21:56:06 +0800

    增加alipay，wxplay的sdk到lib中

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,9 @@ Yii::setAlias('@kshopapi', dirname(dirname(__DIR__)) . '/kshopapi');
 Yii::setAlias('@kshopadmin', dirname(dirname(__DIR__)) . '/kshopadmin');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@OSS', '@common/lib/alisdk/OSS');
+
+require(dirname(__DIR__) . '/lib/wxsdk/wxpay/lib/WxPay.Config.php');
+require(dirname(__DIR__) . '/lib/wxsdk/wxpay/lib/WxPay.Exception.php');
+require(dirname(__DIR__) . '/lib/wxsdk/wxpay/lib/WxPay.Data.php');
+require(dirname(__DIR__) . '/lib/wxsdk/wxpay/lib/WxPay.Notify.php');
+require(dirname(__DIR__) . '/lib/wxsdk/wxpay/lib/WxPay.Api.php');
