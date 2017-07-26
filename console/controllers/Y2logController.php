f3f9e61c7317e4a0330850fb03f95990bbc3ab81
kz <784248377@qq.com>
2017-07-26 17:56:18 +0800

    修改读取日志能够支持远程文件
    1. 在console的config中的params中增减params-local.php文件,
    2. 增加以下配置
    ```
        'y2log_alias' => [
            '@rsfr' => ''@45tpsbk' => 'ssh2.sftp://user:pass@host:port/data/www/trstps-server/backend',',
          ]
    ```
    3. 使用的时候./yii y2log 1m @rsfr 即可输出该日志最后一分钟的内容

--- console/controllers/Y2logController.php
+++ console/controllers/Y2logController.php
@@ -9,7 +9,15 @@ use yii\helpers\VarDumper;
 
 class Y2logController extends Controller{
 
-    public function actionIndex($last = "30s", $app = 'app', $logFile = 'app.log'){
+    public function init(){
+        parent::init();
+        if(!empty(Yii::$app->params['y2log_alias'])){
+            foreach(Yii::$app->params['y2log_alias'] as $name => $path){
+                Yii::setAlias($name, $path);
+            }
+        }
+    }
+    public function actionIndex($last = "30s", $app = '@app', $fileLog = 'app.log'){
         date_default_timezone_set("Asia/Shanghai");
         if(preg_match('/^([0-9]+)([smhd]{1})/', $last, $matches)){
             $timeUnit = $matches[2];
@@ -37,13 +45,27 @@ class Y2logController extends Controller{
             $this->help();
             exit();
         }
-        $file = Yii::getAlias(sprintf('@%s/runtime/logs/%s', $app, $logFile));
+        $file = Yii::getAlias('@' == $app[0] ? sprintf('%s/runtime/logs/%s', $app, $fileLog) : $app);
         $tmpFile = '/tmp/y2log_' . time() . '.txt';
-        $tmpFp = fopen($tmpFile, 'a+');
+        touch($tmpFile);
+        $tmpFp = fopen($tmpFile, 'r+');
+        if(substr_compare($file, 'ssh2.sftp', 0, 8) == 0){
+            $urlInfo = parse_url($file);
+            $session = ssh2_connect($urlInfo['host'], $urlInfo['port']);
+            ssh2_auth_password($session, $urlInfo['user'], $urlInfo['pass']);
+            $stream = ssh2_sftp($session);
+            $remoteFile = "ssh2.sftp://$stream" . $urlInfo['path'];
+            $file = Yii::getAlias('@app/runtime/y2log.txt');
+            file_put_contents($file, file_get_contents($remoteFile));
+            chmod($file, 0600);
+            unset($stream);
+            unset($session);
+        }
         $fp = fopen($file, 'r');
         $pos = -2; // Skip final new line character (Set to -1 if not present)
         $currentLine = '';
         $oneException = [];
+
         while (-1 !== fseek($fp, $pos, SEEK_END)) {
             $char = fgetc($fp);
             if (PHP_EOL == $char) {
@@ -54,7 +76,7 @@ class Y2logController extends Controller{
                         $this->fwrite_stream($tmpFp, implode("\n", $oneException) . "\n");
                         $oneException = [];
                     }else{
-                        return ;
+                        break ;
                     }
                 }else{
                     array_unshift($oneException, $currentLine);
@@ -105,7 +127,8 @@ class Y2logController extends Controller{
             array_unshift($oneException, $currentLine);
         }
         fclose($tmpFp);
-        unlink($tmpFile);
+
+        // unlink($tmpFile);
     }
 
     public function fwrite_stream($fp, $string) {
