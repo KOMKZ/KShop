a5d6144b45cac498a9b4b4a137c29f12a7a700df
kz <784248377@qq.com>
2017-08-02 17:00:46 +0800

    增加errono const类用于统一管理错误代码

---
+++ common/models/staticdata/Errno.php
@@ -0,0 +1,12 @@
+<?php
+namespace common\models\staticdata;
+
+use yii\base\Object;
+/**
+ *
+ */
+class Errno extends Object
+{
+    CONST EXCEPTION = 'EXCEPTION';
+    CONST DB_FAIL_INSERT = 'DB_FAIL_INSERT';
+}
