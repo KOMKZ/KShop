b10b0f3c6a0c9432811834c1ff503fb097983c6d
kz <m13715194169_1@163.com>
2018-06-11 22:31:40 +0800

    fix: add gitignore

--- lshopadmin/web/assets/942d7fcb/js/array-obj-input.js
+++
@@ -1,9 +0,0 @@
-(function( jQuery ) {
-    jQuery(document).ready(function(){
-        jQuery(document).on('click', '.array-obj-input-add', function(e){
-            e.preventDefault();
-            alert(1);
-            return false;
-        });
-    })
-})(jQuery)
